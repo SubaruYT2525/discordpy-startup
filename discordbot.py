a29b42c35331d36a060a016e8ea47c7f0df23083
SubaruYT2525 <71592481+SubaruYT2525@users.noreply.github.com>
2020-10-19 02:58:13 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -1,8 +1,23 @@
+import random
 from discord.ext import commands
-import os
+import asyncio
 import traceback
+import discord
+import inspect
+import textwrap
+import importlib
+from contextlib import redirect_stdout
+import io
+import os
+import re
+import sys
+import copy
+import time
+import typing
+from datetime import datetime
 
-bot = commands.Bot(command_prefix='/')
+
+bot = commands.Bot(command_prefix=('s!','s.'))
 token = os.environ['DISCORD_BOT_TOKEN']
 
 
@@ -14,8 +29,29 @@ async def on_command_error(ctx, error):
 
 
 @bot.command()
+async def sinfo(ctx):
+    embed = discord.Embed(title=f'{ctx.guild.name}の情報', color=0x404040)
+    embed.add_field(name=f'サーバー人数:{len(ctx.guild.members)}人', value=f'実行者:<@{ctx.author.id}>')
+    embed.set_thumbnail(url=ctx.guild.icon_url)
+    await ctx.send(embed=embed)
+
+@bot.command()
+async def say(ctx, *, message:discord.ext.commands.clean_content()):
+    await ctx.send(message)
+    await ctx.message.delete()
+
+@bot.command()
 async def ping(ctx):
-    await ctx.send('pong')
+    embed = discord.Embed(title='pingの測定結果',  color=0x000000)
+    embed.add_field(name=f'{ctx.bot.latency * 1000} ms', value='PONG!')
+    await ctx.send(embed=embed)
+
+
+@bot.event
+async def on_ready():
+    activity = discord.Game(name="起動中", type=3)
+    await bot.change_presence(status=discord.Status.idle, activity=activity)
+    print("Bot is ready!")
 
 
 bot.run(token)
